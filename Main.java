e9c8189526051594a0d10b5bd35148b7faf4cd13
Kshitijkc <kshitijkumarchoudhary>
2019-12-03 22:11:52 +0530

    Update to index.html for using only \s and \n for spilting and update to Main.java for word Not Found! exception and using Stream api instead of Scanner Class for reading file

--- Main.java
+++ Main.java
@@ -1,52 +1,61 @@
 import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.*;
+import java.util.stream.*; 
 import java.util.*;
 
 class Main {
   public static void main(String[] args) throws Exception {
-
-    long start = System.currentTimeMillis();
-    File file = new File("para.txt");
-    Scanner sc = new Scanner(file);
+    
+    long startTime = System.currentTimeMillis();
 
     HashMap<String, PriorityQueue<Integer>> map = new HashMap();
 
-    String line;
-    int count = 0;
+    String[] paragraphs = readLineByLineJava8("para.txt").trim().toLowerCase().split("\n\n+");
 
-    while(sc.hasNextLine()) {
-      line = sc.nextLine().toLowerCase();
-      if(line.equals("")) {
-        continue;
-      }
-      ++count;
-      String[] tokens = line.split("[(\\s+)!?(),;:.]");
-      // System.out.print("\nParagraph : "+ count + " Word Count : " + tokens.length + "\n");
-      for(int j = 0; j < tokens.length; j++) {
-        // System.out.print(tokens[j] + " ");
-        if(!map.containsKey(tokens[j])) {
-          map.put(tokens[j], new PriorityQueue<Integer>());
-          map.get(tokens[j]).add(count);
-        } else if(!map.get(tokens[j]).contains(count)) {
-          map.get(tokens[j]).add(count);
+    for(int paragraph = 0; paragraph < paragraphs.length; paragraph++) {
+      String[] words = paragraphs[paragraph].split("[\\s+\n]");
+      for(int j = 0; j < words.length; j++) {
+        if(!map.containsKey(words[j])) {
+          map.put(words[j], new PriorityQueue<Integer>());
+          map.get(words[j]).add(paragraph + 1);
+        } else if(!map.get(words[j]).contains(paragraph + 1)) {
+          map.get(words[j]).add(paragraph + 1);
         }
       }
-      // System.out.println();
     }
     
-    System.out.println("\n\nTotal Paragraphs : " + count + "\nTotal Unique Words : " + map.size());
-    sc = new Scanner(new File("word.txt"));
+    long searchTimeStart = System.currentTimeMillis();
+    HashSet<String> printedWord = new HashSet<>();
+    String[] words = readLineByLineJava8("word.txt").trim().toLowerCase().split("[\n\\s]+");
+    System.out.println("\n\nTotal Paragraphs : " + paragraphs.length + "\nTotal Unique Words : " + map.size());
     System.out.println("\n\nResult for searched words are as follows : \n------------------------------------------\n");
-    while(sc.hasNext()) {
-      line = sc.next().toLowerCase();
-      // System.out.println(line + " : " + map.get(line) + "\n\n");
-      //                          OR
-      System.out.print(line + " : "); 
-      PriorityQueue<Integer> temp = map.get(line);
-      for(int i = 0; i < temp.size() && i < 10; i++) {
-        System.out.print(temp.remove() + ", ");
-      } System.out.print("\b\b\n\n");
+    for(String word : words) {
+      if(!printedWord.contains(word)) {
+        printedWord.add(word);
+        System.out.print(word + " : "); 
+        PriorityQueue<Integer> temp = map.get(word);
+        if(temp == null) {
+          System.out.print("Not Found!\n\n");
+          continue; 
+        }
+        for(int i = 0; i < temp.size() && i < 10; i++) {
+          System.out.print(temp.remove() + ", ");
+        } System.out.print("\b\b\n\n");
+      }
     }
   
-    System.out.println("Total execution time : " + (System.currentTimeMillis() - start)/1000.0 + "ms");
+    System.out.println("Total execution time : " + (System.currentTimeMillis() - startTime)/1000.0 + "ms\nTotal search time through index : " + (System.currentTimeMillis() - searchTimeStart)/1000.0 + "ms.");
+  }
+
+  private static String readLineByLineJava8(String filePath) 
+  {
+    StringBuilder contentBuilder = new StringBuilder();
+    try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8)) 
+    {
+      stream.forEach(s -> contentBuilder.append(s).append("\n"));
+    }
+    catch (IOException e) {}
+    return contentBuilder.toString();
   }
 }
